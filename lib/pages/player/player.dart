d3a9141e8b2faa303fecd2e9412a69db65268e7d
Roman <rsukner@gmail.com>
2021-02-10 13:18:46 +0200

    added -> player; wav converted to mp3; android minSdkVersion raised to 23

--- lib/pages/player/player.dart
+++ lib/pages/player/player.dart
@@ -3,6 +3,7 @@ import 'package:inhale/bloc/home_feed_bloc.dart';
 import 'package:inhale/components/app-tab-bar.dart';
 import 'package:inhale/model/home_feed.dart';
 import 'package:inhale/networking/api_response.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 class PlayerPage extends StatefulWidget {
   PlayerPage({Key key, this.title}) : super(key: key);
@@ -15,11 +16,20 @@ class PlayerPage extends StatefulWidget {
 
 class _PlayerPageState extends State<PlayerPage> {
   HomeFeedBloc _bloc;
+  AssetsAudioPlayer get _assetsAudioPlayer => AssetsAudioPlayer.withId("music");
 
   @override
   void initState() {
     super.initState();
+
     _bloc = HomeFeedBloc();
+  }
+
+  @override
+  void dispose() {
+    _assetsAudioPlayer.dispose();
+    print("dispose");
+    super.dispose();
   }
 
   @override
@@ -47,6 +57,7 @@ class _PlayerPageState extends State<PlayerPage> {
               case Status.COMPLETED:
                 return PlayerPageView(
                   homeFeed: snapshot.data.data,
+                  assetsAudioPlayer: _assetsAudioPlayer,
                 );
                 break;
               case Status.ERROR:
@@ -69,7 +80,8 @@ class _PlayerPageState extends State<PlayerPage> {
 
 class PlayerPageView extends StatelessWidget {
   final HomeFeed homeFeed;
-  const PlayerPageView({this.homeFeed});
+  final AssetsAudioPlayer assetsAudioPlayer;
+  const PlayerPageView({this.homeFeed, this.assetsAudioPlayer});
 
   @override
   Widget build(BuildContext context) {
@@ -102,17 +114,43 @@ class PlayerPageView extends StatelessWidget {
         Flexible(
           flex: 2,
           child: SizedBox.expand(
-            child: Container(
-              color: Colors.amber,
-              child: Text("player control"),
-            ),
+            child: Container(child: assetsAudioPlayer
+                .builderRealtimePlayingInfos(builder: (context, infos) {
+              return Slider(
+                min: 0,
+                max: infos == null ? 1000 : infos.duration.inSeconds.toDouble(),
+                value: assetsAudioPlayer.currentPosition.value.inSeconds
+                    .toDouble(),
+                onChanged: (newValue) => {
+                  assetsAudioPlayer
+                      .seek(Duration(minutes: 0, seconds: newValue.toInt()))
+                },
+              );
+            })),
           ),
         ),
         Flexible(
           flex: 2,
           child: SizedBox.expand(
-            child:
-                Container(child: Align(child: Image.asset('assets/pause.png'))),
+            child: Container(
+                child: Align(
+                    child: GestureDetector(
+              onTap: () => {
+                if (assetsAudioPlayer.isPlaying.value)
+                  {assetsAudioPlayer.pause()}
+                else if (assetsAudioPlayer.playerState.value ==
+                    PlayerState.pause)
+                  {assetsAudioPlayer.play()}
+                else
+                  {assetsAudioPlayer.open(Audio("assets/under_the_sea.mp3"))}
+              },
+              child: assetsAudioPlayer.builderIsPlaying(
+                  builder: (context, isPlaying) {
+                return isPlaying
+                    ? Image.asset('assets/pause.png')
+                    : Image.asset('assets/play.png');
+              }),
+            ))),
           ),
         ),
       ],
@@ -126,7 +164,27 @@ class PlayerPageViewLoading extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         mainAxisSize: MainAxisSize.max,
-        children: <Widget>[Text("data")],
+        children: <Widget>[
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(height: 24),
+                CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.lightBlue),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/repository/home_feed_repository.dart
+++ lib/repository/home_feed_repository.dart
@@ -5,7 +5,7 @@ import 'package:inhale/model/home_feed.dart';
 
 class HomeFeedRepository {
   Future<HomeFeed> getHomeFeed() async {
-    await new Future.delayed(const Duration(seconds: 1));
+    await new Future.delayed(const Duration(milliseconds: 500));
     String jsonString = await rootBundle.loadString("assets/home_feed.json");
     final jsonResponse = jsonDecode(jsonString);
     return HomeFeed.fromJson(jsonResponse);
